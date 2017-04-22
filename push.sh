7d5668fe07856364b4c3c346a478806ed81fecc9
Geert Baeke <geertbaeke@Geerts-MacBook-Air.local>
2017-04-23 00:19:03 +0200

    push latest

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker push $IMAGE_NAME:$BRANCH.$BUILD_NUMBER
+  #sudo docker push $IMAGE_NAME:$BRANCH.$BUILD_NUMBER
+  sudo docker push $IMAGE_NAME:latest
 else
   echo "skipping because it's a PR"
 fi

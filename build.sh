a1086792c73fb09e1319bb35f7b39be869d013fe
Geert Baeke <geertbaeke@Geerts-MacBook-Air.local>
2017-04-23 00:22:52 +0200

    build latest

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker build -t $IMAGE_NAME:$BRANCH.$BUILD_NUMBER .
+  sudo docker build -t $IMAGE_NAME:latest .
 else
   echo "skipping because it's a PR"
 fi
